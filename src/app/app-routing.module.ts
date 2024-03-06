6633348caae62f2d99920998414f8f0cbd23f8f5
Yaroslav <kogutyaroslav3947@gmail.com>
2024-03-06 18:04:40 -0500

    iml basic compoments

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './features/public/about/about.component';
+import { ServicesComponent } from './features/public/services/services.component';
+import { HomeComponent } from './features/public/home/home.component';
+import { ContactUsComponent } from './features/public/contact-us/contact-us.component';
+import { PortfolioComponent } from './features/public/portfolio/portfolio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'services',
+    component: ServicesComponent
+  },
+  {
+    path: 'contact-us',
+    component: ContactUsComponent
+  },
+  {
+    path: 'portfolio',
+    component: PortfolioComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './core/components/navbar/navbar.component';
+import { AboutComponent } from './features/public/about/about.component';
+import { ServicesComponent } from './features/public/services/services.component';
+import { HomeComponent } from './features/public/home/home.component';
+import { PortfolioComponent } from './features/public/portfolio/portfolio.component';
+import { ContactUsComponent } from './features/public/contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AboutComponent,
+    ServicesComponent,
+    HomeComponent,
+    PortfolioComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule,
