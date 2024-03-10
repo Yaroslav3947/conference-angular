b32dda9c65245314432503318b2ee84b9e4065bc
Yaroslav <kogutyaroslav3947@gmail.com>
2024-03-10 12:15:08 -0400

    moved HomeComponent to PortfolioComponent

--- src/app/features/public/home/home.component.ts
+++ src/app/features/public/home/home.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { UnsplashService } from '../service/unsplash.service';
 
 @Component({
   selector: 'app-home',
@@ -7,22 +6,8 @@ import { UnsplashService } from '../service/unsplash.service';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  photos: any[] = [];
 
-  constructor(private unsplashService: UnsplashService) {
-    this.getPhotos('conference,speaker', 1, 50, 'relevant');
-
-    console.log(this.photos);
-  }
-
-  getPhotos(query: string, page: number, perPage: number, orderBy: string): void {
-    this.unsplashService.getPhotos(query, page, perPage, orderBy).subscribe((photos) => {
-      this.photos = photos.map((photo) => ({
-        id: photo.id,
-        description: photo.description,
-        urls: photo.urls,
-      }));
-    });
+  constructor() {
   }
 
 
--- src/app/features/public/portfolio/portfolio.component.ts
+++ src/app/features/public/portfolio/portfolio.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { UnsplashService } from '../service/unsplash.service';
 
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.css'
 })
-export class PortfolioComponent {
+
+export class HomeComponent {
+  photos: any[] = [];
+
+  constructor(private unsplashService: UnsplashService) {
+    this.getPhotos('conference,speaker', 1, 50, 'relevant');
+
+    console.log(this.photos);
+  }
+
+  getPhotos(query: string, page: number, perPage: number, orderBy: string): void {
+    this.unsplashService.getPhotos(query, page, perPage, orderBy).subscribe((photos) => {
+      this.photos = photos.map((photo) => ({
+        id: photo.id,
+        description: photo.description,
+        urls: photo.urls,
+      }));
+    });
+  }
+
 
 }
